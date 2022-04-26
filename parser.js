eb0b50a5f3793d8c793a45813a669c49c9d976d9
lijayaku <lijayaku@amazon.com>
2022-04-26 13:52:09 -0700

    add download

--- parser.js
+++ parser.js
@@ -2,7 +2,8 @@
  * Copyright 2022 Liju Jayakumar. All Rights Reserved.
 */
 
-function clean(inputText) {
+function clean_up(inputText) {
+
     inputText = inputText.replace(/S/g, "5");
 
     return inputText;
@@ -10,7 +11,7 @@ function clean(inputText) {
 
 function parse() {
 
-    var inputText = clean(document.querySelector('#input-text').value);
+    var inputText = clean_up(document.querySelector('#input-text').value);
     var patters = document.querySelector('#input-patterns').options;
     var output = document.querySelector('#output-text');
 
@@ -33,6 +34,8 @@ function parse() {
             });
         }
     }
+
+    showHideDownload();
 }
 
 function handleFileSelect(event) {
@@ -42,6 +45,46 @@ function handleFileSelect(event) {
 }
 
 function handleFileLoad(event) {
-    console.log(event);
     document.querySelector('#input-text').value = event.target.result;
+}
+
+function clearAll() {
+
+    document.querySelector('#input-text').value = '';
+    document.querySelector('#output-text').value = '';
+    document.querySelector('#fileInput').value = '';
+
+    showHideDownload();
+}
+
+function download() {
+    var textToDownload = document.querySelector('#output-text').value;
+
+    if (textToDownload) {
+        var textFileAsBlob = new Blob([textToDownload], { type: 'text/plain' });
+        var downloadLink = document.createElement("a");
+        downloadLink.download = `phone_numbers_${Date.now()}.txt`;
+        downloadLink.innerHTML = "Download File";
+        if (window.webkitURL != null) {
+            // Chrome allows the link to be clicked
+            // without actually adding it to the DOM.
+            downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+        }
+        else {
+            // Firefox requires the link to be added to the DOM
+            // before it can be clicked.
+            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+            downloadLink.onclick = destroyClickedElement;
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+        }
+
+        downloadLink.click();
+
+    }
+}
+
+function showHideDownload() {
+    const visible = document.querySelector('#output-text').value ? 'visible' : 'hidden';
+    document.querySelector('#output-download').style.visibility = visible;
 }
